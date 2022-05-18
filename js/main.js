1c59550518ab03bb173064497f9e5f482581c3ab
Kiril Kobzar <kirilkobzar@gmail.com>
2022-05-18 22:50:59 +0300

    generate multiple calendars

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-$(document).ready(() => {
-    $('#calendar').datepicker({
-        numberOfMonths: [2, 3]
-    })
+import events from "./events/EVENTS_API.js";
+$(document).ready(async () => {
+    const MONTH_COUNT = 6;
+    const calendarContainer = $('#calendar-container')
+    console.log(events)
+    //generate calendar blocks
+    for (let i = 0; i < MONTH_COUNT; i++){
+        calendarContainer.append(`<div id="calendar_${i}"></div>`)
+        $(`#calendar_${i}`).simpleCalendar({
+            onInit: e => {
+                e.changeMonth(i)
+            }
+        })
+    }
 })
