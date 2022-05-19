6e3460a34c3ba1422380fd079046ead8a5e1741c
Kiril Kobzar <kirilkobzar@gmail.com>
2022-05-19 09:31:39 +0300

    calendar events

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
 import events from "./events/EVENTS_API.js";
-$(document).ready(async () => {
+$(document).ready(() => {
     const MONTH_COUNT = 6;
     const calendarContainer = $('#calendar-container')
-    console.log(events)
     //generate calendar blocks
     for (let i = 0; i < MONTH_COUNT; i++){
+        const eventsByMonth = getEventsByMonth(new Date().setMonth(new Date().getMonth() + i))
+        let calendarEvents = []
+        for (let event of eventsByMonth){
+            calendarEvents.push({
+                startDate: new Date(event.time).toISOString(),
+                endDate: new Date(new Date(event.time).getTime() + 3600).toDateString(),
+                summary: event.details
+            })
+        }
         calendarContainer.append(`<div id="calendar_${i}"></div>`)
         $(`#calendar_${i}`).simpleCalendar({
             onInit: e => {
                 e.changeMonth(i)
-            }
+            },
+            disableEmptyDetails: true,
+            events: calendarEvents
         })
     }
 })
+
+function getEventsByMonth(date = new Date()) {
+    if (date instanceof Date){
+        return events.filter(i => new Date(i.time).getMonth() === date.getMonth())
+    }else {
+        return events.filter(i => new Date(i.time).getMonth() === new Date(date).getMonth())
+    }
+}
